818ba47f1252dd20aa51404aeeb57960aae822c2
zhangxinhao <inkusean@gmail.com>
2022-11-23 14:52:42 +0800

    spring ut

---
+++ java/spring/ut/src/main/java/com/zxh/spring/springut/SpringUtApplication.java
@@ -0,0 +1,13 @@
+package com.zxh.spring.springut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUtApplication.class, args);
+    }
+
+}
---
+++ java/spring/ut/src/test/java/com/zxh/spring/springut/SpringUtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxh.spring.springut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
