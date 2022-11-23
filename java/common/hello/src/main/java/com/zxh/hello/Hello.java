bb52e5d691b520d813823c7308a4b3354884061a
zhangxinhao <inkusean@gmail.com>
2022-11-23 14:40:22 +0800

    java hello

---
+++ java/common/hello/src/main/java/com/zxh/hello/Hello.java
@@ -0,0 +1,9 @@
+package com.zxh.hello;
+
+public class Hello {
+    public static void main(String[] args) {
+        String aaa = "ASDDD";
+        System.out.println(aaa + ": " + aaa.toLowerCase());
+        System.out.printf("Hello!");
+    }
+}
