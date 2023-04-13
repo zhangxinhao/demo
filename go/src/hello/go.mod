f844a08a63e933b17fdb237dd518e3726d1875e4
zhangxinhao <inkusean@gmail.com>
2023-04-13 23:24:17 +0800

    go mod

--- go/src/hello/go.mod
+++ go/src/hello/go.mod
@@ -1,3 +1,10 @@
 module hello
 
 go 1.20
+
+require rsc.io/quote v1.5.2
+
+require (
+	golang.org/x/text v0.3.8 // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
--- go/src/hello/hello.go
+++ go/src/hello/hello.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"hello/morestrings"
+	"rsc.io/quote"
 )
 
+func Hello() string {
+	return quote.Hello()
+}
 func main() {
 	fmt.Println("Hello, world.")
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(Hello())
 }
