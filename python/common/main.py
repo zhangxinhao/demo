32c004d7a058869d7ae9fe9bfc2f927a82710b0f
zhangxinhao <inkusean@gmail.com>
2023-08-16 22:31:02 +0800

    jupyter openai

--- python/common/main.py
+++ python/common/main.py
@@ -12,5 +12,9 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    tmp_str = '''\
+'问题1：\n问：华为OceanStor存储相信什么样的能力可以助力客户应对数字时代的挑战？\n答：华为OceanStor存储相信需要以应需而变的海纳能力，才能融合和识别难以被洞察的数据需求，释放数据智慧。\n\n问题2：\n问：华为OceanStor存储的理念是什么？\n答：华为OceanStor存储秉承“以数据为中心，构建多样化数据应用可靠存储底座”的理念。\n\n问题3：\n问：华为OceanStor存储的能力有哪些特点？\n答：华为OceanStor存储具有融合广泛、灵活应变、绿色环保、面向未来的能力。\n\n问题4：\n问：华为OceanStor存储的目标是什么？\n答：华为OceanStor存储的目标是助力客户从容应对数字时代的多重挑战，实现利于当下、着眼未来的永续发展。'
+    '''
+    print(tmp_str)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
