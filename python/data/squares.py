21d31c73ad1ae9747b8ae8d74ffc8cc0afd5b313
zhangxinhao <inkusean@gmail.com>
2023-08-23 21:35:56 +0800

    plt draw bar

--- python/data/squares.py
+++ python/data/squares.py
@@ -10,5 +10,14 @@ def draw():
     plt.show()
 
 
+# 画一个柱状图，显示1-5的平方
+def draw_bar():
+    x = np.arange(1, 6)
+    y = x ** 2
+    plt.bar(x, y)
+    plt.show()
+
+
 if __name__ == '__main__':
     draw()
+    draw_bar()
