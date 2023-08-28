8e9fcd544b53346ca11610c2e6595e9d57003ca3
zhangxinhao <inkusean@gmail.com>
2023-08-28 23:28:27 +0800

    python copy file

---
+++ python/common/copy_file.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+
+
+def move_files(source_dir, target_dir):
+    # 使用os.listdir来获取源目录中的所有文件和文件夹
+    for filename in os.listdir(source_dir):
+        # 使用os.path.join来构造完整的文件路径
+        full_file_path = os.path.join(source_dir, filename)
+
+        # 使用os.path.isfile来检查这是否是一个文件
+        # 使用str.endswith来检查文件名是否以.md结尾
+        if os.path.isfile(full_file_path) and filename.endswith('.pdf'):
+            # 使用shutil.move来移动文件到目标目录
+            shutil.copy(full_file_path, target_dir)
+
+
+def change_file_name(directory):
+    # 使用 os.listdir 获取目录中的所有文件
+    for filename in os.listdir(directory):
+        # 使用 os.path.join 构造完整的文件路径
+        full_file_path = os.path.join(directory, filename)
+
+        # 使用 os.path.isfile 检查这是否是一个文件
+        if os.path.isfile(full_file_path):
+            new_filename = filename.replace("【海量资源：666java.com】", "")
+            # 使用 os.rename 来修改文件名
+            os.rename(full_file_path, os.path.join(directory, new_filename))
+
+
+if __name__ == '__main__':
+    # 调用函数，将当前目录下的txt文件移动到目标目录
+    move_files('/Users/zhangxinhao/Downloads/rust/161rust', '/Users/zhangxinhao/Downloads/rust/pdf')
+    # change_file_name('/Users/zhangxinhao/Downloads/rust/md')
