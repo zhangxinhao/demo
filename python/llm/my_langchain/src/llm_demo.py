b0dc6bdaf57483818ba1533ce1945e254307a785
zhangxinhao <inkusean@gmail.com>
2025-10-20 19:07:35 +0800

    [python]uv

--- python/llm/my_langchain/src/llm_demo.py
+++ python/llm/my_langchain/src/llm_demo.py
@@ -1,10 +1,15 @@
+import os
+from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
+
+# 加载 .env 文件
+load_dotenv()
 
 llm = init_chat_model(
     model="claude-sonnet-4-5-20250929",
     model_provider="anthropic",
     base_url="https://globalai.vip",  # 自定义 base URL
-    api_key="sk-SWA3Td8L6b6nPHR5yU5zBc2lIUDGafWFbx57RJzkH4JZ77kr"
+    api_key=os.getenv("ANTHROPIC_API_KEY")
 )
 
 if __name__ == '__main__':
